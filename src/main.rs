b786a414058a443b0422c22e633e924bfae0c9d0
Xithrius <xithrius@gmail.com>
2021-12-01 00:44:10 -0800

    Added basic parser for input file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello world!");
+    let input = std::fs::read_to_string("./")
+        .unwrap()
+        .split('\n')
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    // println!("{:?}", input);
+
+    solution1(&input);
+    solution2(&input);
 }
+
+fn solution1(lines: &[_]) {}
+
+fn solution2(lines: &[_]) {}
