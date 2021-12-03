88e14ce0f51575b750f7eb6f511b9c34ca07581b
Xithrius <xithrius@gmail.com>
2021-12-02 20:32:57 -0800

    Added more base code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
+// use Regex::{Captures, Regex};
+// const pattern: &str = r"^$";
+
+// Probably usize, String, or Regex::Captures
+type Item = String;
+
 fn main() {
-    let input = std::fs::read_to_string("./")
+    // let re = Regex::new(pattern).unwrap();
+
+    let input = std::fs::read_to_string("./input.txt")
         .unwrap()
         .split('\n')
-        .map(|s| s.to_string())
-        .collect::<Vec<String>>();
+        .filter_map(|s| s.parse::<Item>().ok())
+        // .flat_map(|s| re.captures(s))
+        .filter(|s| !s.is_empty())
+        .collect::<Vec<Item>>();
 
-    // println!("{:?}", input);
+    // println!("{:#?}", input);
 
     solution1(&input);
     solution2(&input);
 }
 
-fn solution1(lines: &[_]) {}
+fn solution1(lines: &[Item]) {}
 
-fn solution2(lines: &[_]) {}
+fn solution2(lines: &[Item]) {}
